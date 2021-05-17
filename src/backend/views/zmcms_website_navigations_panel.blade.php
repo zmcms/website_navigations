fcce935d9621d863e2977fec47ed2f64362cda67
zmcms <59611848+zmcms@users.noreply.github.com>
2021-05-17 18:28:54 +0200

    Update zmcms_website_navigations_panel.blade.php

--- src/backend/views/zmcms_website_navigations_panel.blade.php
+++ src/backend/views/zmcms_website_navigations_panel.blade.php
@@ -1,20 +1,17 @@
 @extends('themes.zmcms.backend.main')
 @section('content')
 <h1 class="">Zarządzanie nawigacją w serwisie www</h1>
-<div id="zmcms_website_navigations_control_panel" class="control_belt">
+<div id="zmcms_website_navigations_control_panel" class="control_belt" style="text-align: right;">
 	<div class="micro12 ">
 	@foreach($data['positions'] as $p)
 	<button id="zmcms_website_navigations_tab_{{$loop->iteration}}" data-position="{{$p->position}}">{{$p->name}}</button>
 	@endforeach
 	<button id="btn_positions_list"><span class="fas fa-tasks"></span></button>
-	{{-- <button id="btn_positions_new"><span class="fas fa-plus"></span></button> --}}
-	</div>
-</div>
-<div id="zmcms_website_navigations_control_panel" class="control_subbelt_belt">
 	<button id="btn_navigations_new_link"><span class="fas fa-plus"></span> Nowy link</button>
+	</div>
+	
 </div>
 <div id="zmcms_website_navigations_control_panel_content">
 	
 </div>
-{{-- <pre>{{print_r($data, true)}}</pre> --}}
 @endsection
